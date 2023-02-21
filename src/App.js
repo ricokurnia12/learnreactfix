ea9be32ce3ec56ef2468b4dbda85bd4db7fe8747
COCO <ricokurnia19@gmail.com>
2023-02-21 07:19:30 +0700

    post data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './fetch/Product';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Product/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
